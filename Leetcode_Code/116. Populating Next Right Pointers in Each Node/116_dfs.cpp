29d6ffc81968a9ccb795bc09e2b5bd82eaeda5f9
Robert Bogan Kang <bob.kang@qq.com>
2017-05-23 02:24:41 -0500

    Create 116_dfs.cpp

---
+++ Leetcode_Code/116. Populating Next Right Pointers in Each Node/116_dfs.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        connect_dfs(root, 0);
+    }
+
+private:
+    vector<TreeLinkNode *> treeLNheads;
+
+    void connect_dfs(TreeLinkNode *root, int layer) {
+        if (!root) return;
+        if (treeLNheads.size() <= layer)
+            treeLNheads.push_back(root);
+        else {
+            treeLNheads[layer]->next = root;
+            treeLNheads[layer] = root;
+        }
+        connect_dfs(root->left, layer + 1);
+        connect_dfs(root->right, layer + 1);
+    }
+};
