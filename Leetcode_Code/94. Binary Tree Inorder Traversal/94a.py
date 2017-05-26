5e913ca7922cd5628cef4f9d28431c6d013646f3
Robert Bogan Kang <bob.kang@qq.com>
2017-05-26 03:35:27 -0500

    Create 94a.py

---
+++ Leetcode_Code/94. Binary Tree Inorder Traversal/94a.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.result = []
+
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root:
+            self.inorderTraversal(root.left)
+            self.result.append(root.val)
+            self.inorderTraversal(root.right)
+        return self.result
