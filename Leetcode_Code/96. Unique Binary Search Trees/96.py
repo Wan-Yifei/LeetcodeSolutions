52943da29dde2bf7cd8390d505fb6bb39535a1ad
Robert Bogan Kang <bob.kang@qq.com>
2017-05-26 04:05:27 -0500

    Create 96.py

---
+++ Leetcode_Code/96. Unique Binary Search Trees/96.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [0 for i in range(n + 1)]
+        dp[0] = dp[1] = 1
+        for i in range(2, n + 1):
+            total = 0
+            for j in range(i):
+                total += dp[j] * dp[i - j - 1]
+            dp[i] = total
+        return dp[n]
