a1da82f84aa19a31fc84dfcf515b75af418339e9
Robert Bogan Kang <bob.kang@qq.com>
2017-05-26 03:34:33 -0500

    Create 94a3.cpp

---
+++ Leetcode_Code/94. Binary Tree Inorder Traversal/94a3.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        inorderTraversal_dfs(root);
+        return result;
+    }
+
+private:
+    vector<int> result;
+
+    void inorderTraversal_dfs(TreeNode *root) {
+        if (root) {
+            inorderTraversal_dfs(root->left);
+            result.push_back(root->val);
+            inorderTraversal_dfs(root->right);
+        }
+    }
+};
