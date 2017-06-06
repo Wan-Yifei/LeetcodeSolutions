2f8119567e7198d71da8f25e82399ea0eb65a95e
Robert Bogan Kang <bob.kang@qq.com>
2017-06-06 15:57:00 -0500

    Create 22.cpp

---
+++ Leetcode_Code/22. Generate Parentheses/22.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        if (n == 0) return {};
+        vector<string> result;
+        generateParenthesis_dfs(n, "", 0, 0, result);
+        return result;
+    }
+
+    void generateParenthesis_dfs(int n, string now, int left, int right, vector<string> &result) {
+        if (left == n && right == n) {
+            result.push_back(now);
+            cout << now;
+            return;
+        }
+        if (left < n)
+            generateParenthesis_dfs(n, now + "(", left + 1, right, result);
+        if (left > right)
+            generateParenthesis_dfs(n, now + ")", left, right + 1, result);
+    }
+};
