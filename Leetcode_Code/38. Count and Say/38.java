da04d277eabaa73c56a2ca57945ea9cf1db0af71
Robert Bogan Kang <bob.kang@qq.com>
2017-06-07 22:34:27 -0500

    Create 38.java

---
+++ Leetcode_Code/38. Count and Say/38.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public String countAndSay(int n) {
+        String s = "1";
+        if (n == 1)
+            s = "1";
+        for (int j = 0; j < n - 1; ++j) {
+            Character last = s.charAt(0), now;
+            int count = 0;
+            String sb = "";
+            for (int i = 0; i < s.length(); ++i) {
+                now = s.charAt(i);
+                if (now == last)
+                    count++;
+                else {
+                    sb += (String.valueOf(count) + last);
+                    count = 1;
+                }
+                last = s.charAt(i);
+            }
+            sb += (String.valueOf(count) + last);
+            s = sb;
+        }
+        return s;
+    }
+}
