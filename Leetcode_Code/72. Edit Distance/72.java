32772a7438b40619261dba8f7a462b87c3961d0d
Robert Bogan Kang <bob.kang@qq.com>
2017-06-09 20:38:24 -0500

    Update 72.java

--- Leetcode_Code/72. Edit Distance/72.java
+++ Leetcode_Code/72. Edit Distance/72.java
@@ -11,20 +11,20 @@ public class Solution {
             arrL = word2.toCharArray();
             arrS = word1.toCharArray();
         }
-        int[][] dp = new int[2][maxl + 1];
-        for (int i = 0; i < maxl + 1; ++i) {
+        int[][] dp = new int[2][minl + 1];
+        for (int i = 0; i < minl + 1; ++i) {
             dp[0][i] = i;
         }
-        for (int i = 0; i < arrS.length; ++i) {
+        for (int i = 0; i < arrL.length; ++i) {
             dp[(i + 1) % 2][0] = i + 1;
-            for (int j = 0; j < arrL.length; ++j) {
-                if (arrL[j] != arrS[i])
+            for (int j = 0; j < arrS.length; ++j) {
+                if (arrL[i] != arrS[j])
                     dp[(i + 1) % 2][j + 1] = min3(dp[(i + 1) % 2][j], dp[i % 2][j], dp[i % 2][j + 1]) + 1;
                 else
                     dp[(i + 1) % 2][j + 1] = dp[i % 2][j];
             }
         }
-        return dp[arrS.length % 2][arrL.length];
+        return dp[(arrL.length + 0) % 2][arrS.length];
     }
 
     private int min3(int a, int b, int c) {
