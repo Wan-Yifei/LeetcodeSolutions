015fbf0476709a2187d771edfea8fb7f81cf8689
Robert Bogan Kang <bob.kang@qq.com>
2017-06-12 00:27:13 -0500

    Update 151.java

--- Leetcode_Code/151. Reverse Words in a String/151.java
+++ Leetcode_Code/151. Reverse Words in a String/151.java
@@ -2,17 +2,26 @@ public class Solution {
     public String reverseWords(String s) {
         if (s.length() == 0 || s == null)
             return "";
-        int idx = 0;
+        int idx = 0, index = 0;
         StringBuilder result = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; --i) {
-            char c = s.charAt(i);
-            if (c != ' ') {
-                result.insert(idx, c);
+            if (s.charAt(i) != ' ') {
+                index++;
+                result.insert(idx, s.charAt(i));
             } else {
+                while (i >= 0 && s.charAt(i) == ' ') {
+                    i--;
+                }
+                if (i < 0)
+                    break;
+                i++;
                 result.append(' ');
-                idx = s.length() - i;
+                idx = index + 1;
+                index++;
             }
         }
+        if (result.length() > 1 && result.charAt(0) == ' ')
+            result.deleteCharAt(0);
         return String.valueOf(result);
     }
 }
