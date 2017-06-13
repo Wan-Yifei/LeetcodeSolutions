e04fbeedc06247fd97bd7be8baaa11c76fd50399
Robert Bogan Kang <bob.kang@qq.com>
2017-06-13 13:43:57 -0500

    Create 273.java

---
+++ Leetcode_Code/273. Integer to English Words/273.java
@@ -0,0 +1,53 @@
+public class Solution {
+    public String numberToWords(int num) {
+        if (num == 0)
+            return "Zero";
+        List<String> resArr = new ArrayList<>();
+        int index = 0;
+        while (num != 0) {
+            int threeDigits = num - num / 1000 * 1000;
+            resArr.add(0, moreArr[index]);
+            resArr.addAll(0, smallNumberToWords(threeDigits));
+            num /= 1000;
+            index++;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < resArr.size(); ++i) {
+            if (resArr.get(i) != "") {
+                result.append(resArr.get(i));
+                result.append(" ");
+            }
+        }
+        result.deleteCharAt(result.length() - 1);
+        return result.toString();
+    }
+
+    private String[] onesArr = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+    private String[] teensArr = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourneen", "Fifteen", "Sixteen",
+            "Seventeen", "Eighteen", "Nineteen" };
+    private String[] tensArr = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
+            "Ninety" };
+    private String[] moreArr = { "", "Thousand", "Million", "Billion", "Trillion" };
+
+    private List<String> smallNumberToWords(int num) {
+        int[] digits = new int[3];
+        for (int i = 0; i < 3; ++i) {
+            digits[2 - i] = num - num / 10 * 10;
+            num /= 10;
+        }
+        List<String> resArr = new ArrayList<>();
+        if (digits[0] != 0) {
+            resArr.add(onesArr[digits[0]]);
+            resArr.add("Hundred");
+        }
+        if (digits[1] == 1) {
+            resArr.add(teensArr[digits[2]]);
+        } else {
+            if (digits[1] > 1) {
+                resArr.add(tensArr[digits[1]]);
+            }
+            resArr.add(onesArr[digits[2]]);
+        }
+        return resArr;
+    }
+}
