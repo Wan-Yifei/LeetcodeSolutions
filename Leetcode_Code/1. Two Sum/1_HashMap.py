cc4993755e65356d2bd7b91394e0f35a14f6456c
Robert Bogan Kang <bob.kang@qq.com>
2018-03-27 17:29:41 -0500

    Create 1_HashMap.py

---
+++ Leetcode_Code/1. Two Sum/1_HashMap.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mp = {}
+        for i in range(len(nums)):
+            v = nums[i]
+            if (target - v in mp) and (mp[target - v] >= 0):
+                return [mp[target - v], i]
+            else:
+                mp[v] = i
